d16dae1c350bc30c13cbfa70ac72be131b5d2eca
shahdatef345 <shahdatef346@gmail.com>
2024-12-03 19:13:53 +0200

    Edit

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4cc0813fb08c4dab3ac6c7b68534f200cbce62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnlineCenter/obj/Debug/net8.0/OnlineCenter.AssemblyInfo.cs
+++ OnlineCenter/obj/Debug/net8.0/OnlineCenter.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4cc0813fb08c4dab3ac6c7b68534f200cbce62")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
